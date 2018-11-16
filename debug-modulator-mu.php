961a152fe9f22e070b0b309b2b29ab67e6fe0415
Gary Kovar <gkovar@gmail.com>
2018-11-16 10:49:06 -0500

    hooks late enough to override wp error log

--- debug-modulator-mu.php
+++ debug-modulator-mu.php
@@ -4,7 +4,9 @@ require WP_PLUGIN_DIR . '/debug-modulator/src/Logging/Collector.php';
 
 function debug_modulator_startup() {
 	$collector = new \Modulator\Logging\Collector();
-	set_error_handler( [ $collector, 'error_handler' ] );
+	add_action( 'plugins_loaded', function() use ($collector) {
+		set_error_handler( [ $collector, 'error_handler' ] );
+	} ) ;
 	register_shutdown_function( [ $collector, 'shutdown_handler'] );
 }
 debug_modulator_startup();
